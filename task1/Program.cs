179f10b6aaa1bce75695c80aa492b1caf0d34a5d
diplg <dimas4354@icloud.com>
2022-05-23 23:28:55 +0300

    двумерный массив

---
+++ task1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+int m = 5;
+int n = 4;
+
+int[,] mass = new int [m , n];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i,j] = new Random().Next(-10,10);
+        Console.Write(mass[i,j] + " ");
+    }
+    Console.WriteLine();                            // двумерный массив заполненый целыми числами
+}
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+       mass[i, j] = mass[i, j] * mass[i, j];
+    }
+}                                                     //корень двумерного массива
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+       Console.Write(mass[i,j] + " ");
+    }
+    Console.WriteLine();
+}
