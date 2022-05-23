df3d93ea8513a972c11de16acab7b5a762fefcfe
diplg <dimas4354@icloud.com>
2022-05-24 01:11:21 +0300

    возведение под корень элементов с чётными индексами

---
+++ task3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+int m = 4, n = 3;
+int[,] mass = new int [m, n];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i,j] = new Random().Next(-10, 10);
+    }
+
+}
+void Print(int[,] array);
+{
+    for (int i = 0; i < array.GetLength(0); i++) 
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Print(mass);
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        if(i%2 == 0 & j%2 == 0)
+        {
+            mass[i, j] = mass[i, j] * mass[i, j];
+        }
+    }
+}
+
+Print(mass);
