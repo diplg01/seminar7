45f034f3dc2a052d7ed4e5f355cc694f9c2efcf4
diplg <dimas4354@icloud.com>
2022-05-24 00:13:59 +0300

    двумерный массив по формуле A = m+n

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте массив по вормуле А = м+n
+
+int m = 4, n = 3;
+int[,] mass = new int [m, n];
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = i + j;
+        Console.Write(mass[i, j] + " ");
+    }
+    Console.WriteLine();
+}
